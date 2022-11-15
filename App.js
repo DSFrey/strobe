32345142c45f35547c4b19e38ee42913a09bcb4f
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-11-15 06:48:01 +0000

    Implement strobe, kind of

--- App.js
+++ App.js
@@ -1,25 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { StyleSheet, Switch, Text, View } from 'react-native';
-import { Camera, CameraType } from 'expo-camera';
+import { Camera, CameraType, FlashMode } from 'expo-camera';
 
 export default function App() {
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const [isStrobeOn, setStrobeOn] = useState(false);
   const [strobeSpeed, setStrobeSpeed] = useState(1);
+  const [isTorchOn, setTorchOn] = useState(false)
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    requestPermission()
+  }, [])
 
   const toggleStrobe = () => {
     setStrobeOn(!isStrobeOn)
   }
 
   useEffect(() => {
-    requestPermission()
-  }, [])
+    const interval = isStrobeOn
+      ? setInterval(() => {
+        setCounter(counter => counter + 1)
+        setTorchOn(true)
+        setTimeout(()=>{
+          setTorchOn(false)
+        }, 1000)
+      }, 2000)
+      : setTorchOn(false);
+    return () => clearInterval(interval);
+  }, [isStrobeOn])
 
   return (
     <View style={styles.container}>
       <Text>STROBE</Text>
-      {permission ? <Text>{permission.status}</Text> : null}
+      {permission ? <Text>{`${isTorchOn}, ${counter}`}</Text> : null}
+      <Camera type={CameraType.back} flashMode={isTorchOn ? FlashMode.torch : FlashMode.off} style={{ flex: 1, width: '100%', height: '100%', alignSelf: 'stretch'}}/>
       <Switch
         value={isStrobeOn}
         onValueChange={toggleStrobe}
